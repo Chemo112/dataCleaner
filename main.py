870a6cd5a76503e1131058375a9c020abc436d57
chemo <chemotti@gmail.com>
2023-06-06 16:41:06 +0200

    FIRST COMMIT

--- main.py
+++ main.py
@@ -19,36 +19,20 @@ conn = psycopg2.connect(
 
 engine = sqlalchemy.create_engine('postgresql://' + db_user + ':' + db_password + '@' + db_host + ':' + db_port + '/' + db_name)
 
+
+#here we must implement the csv input from the scaper
 DB = pd.read_csv("edx_courses.csv")
 DB.to_sql('bronze', engine, if_exists='replace', index=False)
 
 lista = [row for row in DB['subject']]
 argomenti = sorted(list(set(lista)))
 
+df = DB[['subject','course_description','Level','course_url']].copy()
+df.to_sql('silver', conn, if_exists='replace', index=False)
 
+df1 = DB[['subject','course_description','Level','course_url']].copy()
 
-def reccomender(subjects_exp):
-    dict_sbj = dict()
-    res = dict()
-    for sbj in subjects_exp:
-        if subjects_exp[sbj] > 0:
-            if subjects_exp[sbj] // 3 <= 1:
-                diff = "Introductory"
-            else:
-                if subjects_exp[sbj] // 3 <= 2:
-                    diff = "Intermediate"
-                else:
-                    diff = "Advanced"
-            dict_sbj[sbj] = diff
-    for sbj in dict_sbj:
-        df = DB.loc[DB['subject'] == sbj]
-        df.fillna('Description is Missing', inplace=True) #fa il replace delle descrizioni mancanti
-        df = df.drop_duplicates() #rimuove righe duplicate
-        df.to_sql('silver', conn, if_exists='replace', index=False)
-        df1 = df.loc[df['Level'] == dict_sbj[sbj]]
-        df1.to_sql('silver', conn, if_exists='replace', index=False)
-
-        res[sbj] = df1
-    return res
-
+df1.fillna(' ', inplace=True)  # fa il replace delle descrizioni mancanti
+df1 = df1.drop_duplicates()  # rimuove righe duplicate
+df1.to_sql('gold', conn, if_exists='replace', index=False)
 
