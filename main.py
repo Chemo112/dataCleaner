21a0382c67feda98c328896618ecff7461dfea2d
chemo <chemotti@gmail.com>
2023-06-06 12:31:15 +0200

    FIRST COMMIT

--- main.py
+++ main.py
@@ -56,4 +56,4 @@ def reccomender(subjects_exp):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
+    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT"))
